abaffcc6ff49500b7fd60fde64d1cf2b6c2f603d
Nate Lapinski <n4t311@gmail.com>
2020-02-06 21:22:59 +0900

    Updates to Voter.sol to allow an address to vote only once, and to vote for an option by string name instead of array index.
    updates to readme.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smart_contracts
-A collection of smart contracts
+A collection of smart contracts.
+
+Voter.sol is based on the examples given in Ivan Mushketyk's Pluralsight course, Developing Applications on Ethereum Blockchain. I have updated it to Solidity v6+, and made some improvements.
--- Voter.sol
+++ Voter.sol
@@ -2,16 +2,42 @@ pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract Voter {
+    struct OptionPos {
+        uint pos;
+        bool exists;
+    }
+    
     uint[] public votes;
     string[] public options;
+    mapping (address => bool) hasVoted;
+    mapping (string => OptionPos) posOfOption;
+    
     constructor(string[] memory _options) public {
         options = _options;
         votes = new uint[](options.length);
+        
+        for (uint i = 0; i < options.length; i++) {
+            OptionPos memory optionPos = OptionPos(i, true);
+            string memory optionName = options[i];
+            posOfOption[optionName] = optionPos;
+        }
     }
     function vote(uint option) public {
         require(0 <= option && option < options.length, "Invalid options");
+        require(!hasVoted[msg.sender], "Account has already voted");
         votes[option] = votes[option] + 1;
+        hasVoted[msg.sender] = true;
     }
+    
+    function vote(string memory optionName) public {
+        require(!hasVoted[msg.sender], "Account has already voted");
+        
+        OptionPos memory optionPos = posOfOption[optionName];
+        require(optionPos.exists, "Option does not exist");
+        votes[optionPos.pos] = votes[optionPos.pos] + 1;
+        hasVoted[msg.sender] = true;
+    }
+    
     function getOptions() public view returns (string[] memory) {
         return options;
     }
